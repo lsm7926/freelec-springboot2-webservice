66c8eb43b55021018972cd313101686e98a32b75
limsungmin <lsm7926@gmail.com>
2021-03-23 22:47:05 +0900

    Initial commit

---
+++ src/main/java/com/lsm7926/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,17 @@
+package com.lsm7926.book.springboot.service.posts;
+import com.lsm7926.book.springboot.domain.posts.PostsRepository;
+import com.lsm7926.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/main/java/com/lsm7926/book/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,10 @@
+package com.lsm7926.book.springboot.web.dto;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+}
